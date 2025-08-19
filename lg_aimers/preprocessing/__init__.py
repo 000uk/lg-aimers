d4aa3ee065d84b604d68e3863b93bc437d407237
suh <goljeol@gmail.com>
2025-08-20 01:22:04 +0900

    불필요한 파일 삭제

--- lg_aimers/preprocessing/__init__.py
+++ lg_aimers/preprocessing/__init__.py
@@ -1,6 +1,5 @@
 from .load_data import load_data
 from .calendar import add_date_features, add_holiday_info
 from .encoders import fit_label_encoders, save_encoders, load_encoders, encode_labels
-from .scalers import sales_scaler
 
 from .notyet.embedding import CategoryEmbeddings
