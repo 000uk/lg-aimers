1c86bad36e9ac4804a77a38e781104e3c7ec6d1b
suh <goljeol@gmail.com>
2025-08-19 21:18:48 +0900

    fix: __init__ 수정

--- lg_aimers/preprocessing/__init__.py
+++ lg_aimers/preprocessing/__init__.py
@@ -1,6 +1,5 @@
 from .static.data_loader import load_data
 from .static.calendar import add_date_features, add_holiday_info
-from .static.zzzz import add_kr_holiday_features
 
 from .fitted.decomposition import stl_decompose
 from .fitted.encoders import fit_label_encoders, save_encoders, load_encoders, encode_labels
