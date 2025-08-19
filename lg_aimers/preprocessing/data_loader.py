35b01be2767d99029d04138045aa148d1a7a1edf
suh <goljeol@gmail.com>
2025-08-19 23:25:03 +0900

    fix: data_loader.py 수정

--- lg_aimers/preprocessing/data_loader.py
+++ lg_aimers/preprocessing/data_loader.py
@@ -4,6 +4,8 @@ from preprocessing import (
 )
 
 def load_data(df):
+    df = df.copy()
+    
     # 컬럼명 영문 변환
     df = df.rename(columns={
     '영업일자': 'date',
@@ -15,12 +17,12 @@ def load_data(df):
     df['store'] = df['store_menu'].str.split('_').str[0]
     df['menu'] =  df['store_menu'].str.split('_').str[1]
 
-    df_train = add_date_features(df_train)
-    df_train = add_holiday_info(df_train)
-    df_train = df_train.drop(columns=['day', 'month', 'dow', 'week', 'day_of_year'])
+    df = add_date_features(df)
+    df = add_holiday_info(df)
+    df = df.drop(columns=['day', 'month', 'dow', 'week', 'day_of_year'])
 
-    le_store, le_menu, le_holiday = fit_label_encoders(df_train)
+    le_store, le_menu, le_holiday = fit_label_encoders(df)
     save_encoders(le_store, le_menu, le_holiday, 'le_store.pkl', 'le_menu.pkl', 'le_holiday.pkl')
-    df_train = encode_labels(df_train, le_store, le_menu, le_holiday)
+    df = encode_labels(df, le_store, le_menu, le_holiday)
 
     return df
