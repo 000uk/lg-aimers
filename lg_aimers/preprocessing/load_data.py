44d54c4cc041ef332f4c26af733f3fa00b6a250f
suh <goljeol@gmail.com>
2025-08-20 00:05:17 +0900

    fix: load_data.py 수정

--- lg_aimers/preprocessing/load_data.py
+++ lg_aimers/preprocessing/load_data.py
@@ -1,8 +1,3 @@
-from preprocessing import (
-    add_date_features, add_holiday_info,
-    MultiLabelEncoder
-)
-
 def load_data(df):
     df = df.copy()
 
@@ -16,25 +11,5 @@ def load_data(df):
     # store/menu 분리 및 전처리
     df['store'] = df['store_menu'].str.split('_').str[0]
     df['menu'] =  df['store_menu'].str.split('_').str[1]
-
-    mle = MultiLabelEncoder() 
-    mle.fit(df, ["store", "menu", "holiday"]) # 학습 데이터로 fit
-    df = mle.transform(df)
-    mle.save("encoders/label")
-
-    # ---------------------------------
-    # 추후 로드할 때
-    mle2 = MultiLabelEncoder()
-    mle2.load("encoders/label", ["store", "menu", "holiday"])
-
-    # 로드한 걸로 새 데이터 변환
-    df_test = mle2.transform(df_test)
-
-
-
-    # 날짜 전처리
-    df = add_date_features(df)
-    df = add_holiday_info(df)
-    df = df.drop(columns=['day', 'month', 'dow', 'week', 'day_of_year'])
 
     return df
