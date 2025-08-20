a7507574c0d11f6527649a7cc3cece45a7191a75
suh <goljeol@gmail.com>
2025-08-20 12:56:59 +0900

    fix: stl 분해서 정규화된 값 참조하도록 수정

--- lg_aimers/stl/stl_decompose.py
+++ lg_aimers/stl/stl_decompose.py
@@ -5,12 +5,12 @@ def stl_decompose(df):
     results = []
 
     for _, group in df.groupby('store_menu'):
-        stl = STL(group['sales_qty'], period=7, robust=True) # 주간 패턴이 아무래도 중요할 거 같아서 7로 함
+        stl = STL(group['sales_norm'], period=7, robust=True) # 주간 패턴이 아무래도 중요할 거 같아서 7로 함
         res = stl.fit()
         group = group.copy()  # 원본 df 건드리지 않기 위해 복사
         group['trend'] = res.trend
         group['seasonal'] = res.seasonal
-        group['residual'] = group['sales_qty'] - (group['trend'] + group['seasonal'])
+        group['residual'] = group['sales_norm'] - (group['trend'] + group['seasonal'])
         results.append(group)
 
     return pd.concat(results, ignore_index=True)
