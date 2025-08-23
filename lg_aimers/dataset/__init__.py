8f233197aeb92e066587b28c5db439a66e21eb17
suh <goljeol@gmail.com>
2025-08-24 03:53:07 +0900

    init 수정

--- lg_aimers/dataset/__init__.py
+++ lg_aimers/dataset/__init__.py
@@ -1,4 +1,3 @@
 # from .custom_dataset import WindowGenerator, TimeSeriesDataset
 from .datasets import WindowGenerator, TSFullDataset
-from .custom_dataset import WindowGenerator2, TimeSeriesDataset
 from .data_split import time_based_split
